ac8e3bdc64299b086080b965fd5b2cdc6847b566
Xueling <xlmeng@asu.edu>
2019-02-25 17:01:13 -0700

    Initial version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,70 @@
+package cse360assign2;
+/**
+ * This class implements a simple calculator structure, calculator could do 
+ * add, subtract, multiply and add operation, display calculated history.
+ * Github repository : https://github.com/xiaohai0313/CSE360assign2_submit.git
+ * 
+ * @author Xueling Meng
+ * @ClassID: 489
+ * @since 2019-2-25
+ */
+public class Calculator {
+
+	private int total;
+	/**
+	 * This method should initial variable
+	 * @return - initial total parameter
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method should return the current total value
+	 * @return - current total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method should add parameter to the total variable
+	 * @param value - the value to be add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method should subtract parameter from total variable
+	 * @param value - the value to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method should multiply the total variable by the parameter
+	 * @param value - the value to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method should divide the total by the parameter, if parameter is zero
+	 * set the total to zero
+	 * @param value - the value to be divided
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * The getHistory method will return a history of all actions as a String.
+	 * @return - calculation history String
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
